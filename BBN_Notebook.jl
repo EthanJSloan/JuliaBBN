ef893d4ba238115be002fa5d5e2fe8ad719ca97b
EthanJSloan <123033468+EthanJSloan@users.noreply.github.com>
2023-06-18 10:58:37 -0600

    Create BBN_Notebook.jl

---
+++ BBN_Notebook.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6a33171e-0df8-11ee-0fc7-5fdc303d429e
+md"# JuliaBBN"
+
+# ╔═╡ 821d46c7-db20-4076-b8e0-50286c5e6ff5
+md"The goal of this project is to create code with similar functionality to [AlterBBN](https://www.sciencedirect.com/science/article/pii/S0010465519303236). I believe that making this code will be more user-friendly and interactive, and will also serve as a more effective way to learn about Big Bang Nucleosynthesis."
+
+# ╔═╡ Cell order:
+# ╟─6a33171e-0df8-11ee-0fc7-5fdc303d429e
+# ╟─821d46c7-db20-4076-b8e0-50286c5e6ff5
